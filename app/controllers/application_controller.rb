fc8dea2310edaa8aaa7d36aa3709598443d85ffb
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-08-20 11:37:02 -0600

    commit tree 3eb1ee2e9634d7e1bf2b5d7787b2cca51f82141b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+   wrap_parameters format:[]
+   
+    def create
+        bird = Bird.create(params)
+        render json: bird, status: :created
+    end
+
+    private
+
+    def bird_params
+        params.permit(:name, :species)
+    end
+
+
 end
